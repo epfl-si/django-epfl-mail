87a26f297f2a584c3ae4d093cf3eddd40eaa4655
William Belle <william.belle@gmail.com>
2021-06-17 15:26:27 +0200

    Add tests

---
+++ tests/test_epflmail_templates.py
@@ -0,0 +1,38 @@
+# (c) ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, DSI, 2021.
+
+from django.core import mail
+from django.core.mail.message import EmailMessage
+from django.template.loader import render_to_string
+from django.test import TestCase
+
+
+class TestsEpflMailTemplates(TestCase):
+    def test_send_epflmail_without_title(self):
+
+        html = render_to_string("actu.html")
+        email = EmailMessage(
+            "Email Actu", html, "from@example.com", ["to@example.com"]
+        )
+        email.send()
+
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].subject, "Email Actu")
+        self.assertInHTML("<p>This is Actu.</p>", mail.outbox[0].body)
+        self.assertIn("logo.png", mail.outbox[0].body)
+        self.assertIn("EPFL, all rights reserved", mail.outbox[0].body)
+        self.assertNotIn("square.png", mail.outbox[0].body)
+
+    def test_send_epflmail_with_title(self):
+
+        html = render_to_string("memento.html", {"APP_TITLE": "Memento"})
+        email = EmailMessage(
+            "Email Memento", html, "from@example.com", ["to@example.com"]
+        )
+        email.send()
+
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].subject, "Email Memento")
+        self.assertInHTML("<p>This is Memento.</p>", mail.outbox[0].body)
+        self.assertIn("logo.png", mail.outbox[0].body)
+        self.assertIn("EPFL, all rights reserved", mail.outbox[0].body)
+        self.assertIn("square.png", mail.outbox[0].body)
